99a01844852bf7e5de8ef85294bf9e7568fb3e4a
PedroSebastiaan <pizarropedrov@gmail.com>
2021-10-08 15:51:13 -0300

    add: payment method

--- db/migrate/20211008183636_create_orders.rb
+++ db/migrate/20211008183636_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.string :amount
+      t.integer :amount
 
       t.timestamps
     end
---
+++ db/seeds/orders.rb
@@ -0,0 +1,4 @@
+Order.destroy_all
+
+user = User.create(name: "John Doe")
+Order.create(user_id: user.id, amount: 10000)
